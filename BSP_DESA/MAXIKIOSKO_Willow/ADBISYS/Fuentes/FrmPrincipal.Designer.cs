774337aca67c45748f077d274ed8683acadb68dc
Rodrigo Ruben Romano <romanorodrigo@ymail.com>
2014-03-22 21:13:33 -0300

    Empezando con el sistemita

---
+++ BSP_DESA/MAXIKIOSKO_Willow/ADBISYS/Fuentes/FrmPrincipal.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace ADBISYS
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StripPrincipal = new System.Windows.Forms.MenuStrip();
+            this.artículosTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ventasTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.cajaTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.proveedoresTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.rubrosTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportesTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.ayudaTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirTS = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStripPrincipal = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.StripPrincipal.SuspendLayout();
+            this.statusStripPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // StripPrincipal
+            // 
+            this.StripPrincipal.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.StripPrincipal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.artículosTS,
+            this.ventasTS,
+            this.cajaTS,
+            this.proveedoresTS,
+            this.rubrosTS,
+            this.reportesTS,
+            this.ayudaTS,
+            this.salirTS});
+            this.StripPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.StripPrincipal.Name = "StripPrincipal";
+            this.StripPrincipal.Padding = new System.Windows.Forms.Padding(8, 3, 0, 3);
+            this.StripPrincipal.Size = new System.Drawing.Size(1444, 30);
+            this.StripPrincipal.TabIndex = 0;
+            this.StripPrincipal.Text = "menuStrip1";
+            // 
+            // artículosTS
+            // 
+            this.artículosTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.artículosTS.Name = "artículosTS";
+            this.artículosTS.Size = new System.Drawing.Size(87, 24);
+            this.artículosTS.Text = "Artículos";
+            // 
+            // ventasTS
+            // 
+            this.ventasTS.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ventasTS.Name = "ventasTS";
+            this.ventasTS.Size = new System.Drawing.Size(73, 24);
+            this.ventasTS.Text = "Ventas";
+            // 
+            // cajaTS
+            // 
+            this.cajaTS.Name = "cajaTS";
+            this.cajaTS.Size = new System.Drawing.Size(55, 24);
+            this.cajaTS.Text = "Caja";
+            // 
+            // proveedoresTS
+            // 
+            this.proveedoresTS.Name = "proveedoresTS";
+            this.proveedoresTS.Size = new System.Drawing.Size(115, 24);
+            this.proveedoresTS.Text = "Proveedores";
+            // 
+            // rubrosTS
+            // 
+            this.rubrosTS.Name = "rubrosTS";
+            this.rubrosTS.Size = new System.Drawing.Size(75, 24);
+            this.rubrosTS.Text = "Rubros";
+            // 
+            // reportesTS
+            // 
+            this.reportesTS.Name = "reportesTS";
+            this.reportesTS.Size = new System.Drawing.Size(89, 24);
+            this.reportesTS.Text = "Reportes";
+            // 
+            // ayudaTS
+            // 
+            this.ayudaTS.Name = "ayudaTS";
+            this.ayudaTS.Size = new System.Drawing.Size(67, 24);
+            this.ayudaTS.Text = "Ayuda";
+            // 
+            // salirTS
+            // 
+            this.salirTS.Name = "salirTS";
+            this.salirTS.Size = new System.Drawing.Size(55, 24);
+            this.salirTS.Text = "Salir";
+            // 
+            // statusStripPrincipal
+            // 
+            this.statusStripPrincipal.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStripPrincipal.Location = new System.Drawing.Point(0, 573);
+            this.statusStripPrincipal.Name = "statusStripPrincipal";
+            this.statusStripPrincipal.Size = new System.Drawing.Size(1444, 25);
+            this.statusStripPrincipal.TabIndex = 1;
+            this.statusStripPrincipal.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1444, 598);
+            this.Controls.Add(this.statusStripPrincipal);
+            this.Controls.Add(this.StripPrincipal);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Maxikosko WILLOW - BSP - Brother\'s System\'s Programmer\'s - ADBISYS 1.0";
+            this.Load += new System.EventHandler(this.FrmPrincipal_Load);
+            this.StripPrincipal.ResumeLayout(false);
+            this.StripPrincipal.PerformLayout();
+            this.statusStripPrincipal.ResumeLayout(false);
+            this.statusStripPrincipal.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip StripPrincipal;
+        private System.Windows.Forms.ToolStripMenuItem artículosTS;
+        private System.Windows.Forms.ToolStripMenuItem ventasTS;
+        private System.Windows.Forms.ToolStripMenuItem cajaTS;
+        private System.Windows.Forms.ToolStripMenuItem proveedoresTS;
+        private System.Windows.Forms.ToolStripMenuItem rubrosTS;
+        private System.Windows.Forms.ToolStripMenuItem reportesTS;
+        private System.Windows.Forms.ToolStripMenuItem ayudaTS;
+        private System.Windows.Forms.ToolStripMenuItem salirTS;
+        private System.Windows.Forms.StatusStrip statusStripPrincipal;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+
+
+
+
+
+    }
+}
+
---
+++ BSP_DESA/MAXIKIOSKO_Willow/ADBISYS/Fuentes/FrmPrincipal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// RR 2014-03-22: Comienzo del sistema ADBISYS.
+
+namespace ADBISYS
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmPrincipal_Load(object sender, EventArgs e)
+        {
+            // RR 2014-03-22 [INICIO]: Ajusta a pantalla completa el Formulario Principal.
+            this.Location = Screen.PrimaryScreen.WorkingArea.Location;
+            this.Size = Screen.PrimaryScreen.WorkingArea.Size;
+            // RR 2014-03-22 [FIN]: Ajusta a pantalla completa el Formulario Principal.
+        }
+
+    }
+}
